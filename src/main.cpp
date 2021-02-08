1dd7c52ac35e47c45a54fa065ff99227bc107e47
Leytzher Muro <leytzher@gmail.com>
2021-02-08 17:54:40 +0000

    boost asio

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,37 @@
 #include <boost/asio.hpp>
 #include <boost/system/error_code.hpp>
+
 #include <iostream>
 
-int main(){
-  boost::system::error_code ec {};
-  if (ec){
-    std::cerr<<"Error: "<< ec.message() << std::endl;
-    return -1;
-  } else {
-    std::cout<<"Ok"<<std::endl;
-    return 0;
-  }
+using tcp = boost::asio::ip::tcp;
+
+void Log(boost::system::error_code ec){
+  std::cerr << (ec ? "Error: " : "OK")
+	    << (ec ? ec.message() : "")
+	    << std::endl;
 }
 
+
+int main()
+{
+  boost::asio::io_context ioc {};
+  tcp::socket socket {ioc};
+
+  boost::system::error_code ec {};
+  tcp::resolver resolver {ioc};
+  auto endpoint {resolver.resolve("google.com", "80", ec)};
+  if (ec) {
+    Log(ec);
+    return -1;
+  }
+  socket.connect(*endpoint, ec);
+  if (ec) {
+    Log(ec);
+    return -2;
+  }
+  Log(ec);
+  return 0;
+
+
+
+}
